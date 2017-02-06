60ab046ae151d3d20375cf3383d414192941afcf
balla28 <ball.balla49@gmail.com>
2017-02-06 15:29:43 +0000

    changes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //Write your function calls below this line
+setColor(0,0, "blue");
+setColor(0,9, "blue");
+setColor(9,9, "blue");
+setColor(9,0, "blue");
+setColor(9,0, "blue");
+setColor(2,3, "red");
+setColor(2,6, "red");
+setColor(4,1, "red");
+setColor(5,2, "red");
+setColor(6,3, "red");
+setColor(6,4, "red");
+setColor(6,5, "red");
+setColor(5,6, "red");
+setColor(4,7, "red");
